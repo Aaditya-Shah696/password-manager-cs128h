041142eefc293038c19e0b6e3878d654021b8da9
Andrei K. Badulescu <abadu2@Illinois.edu>
2024-11-08 14:33:15 -0600

    parse_domain in utils functions

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,23 @@
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 
+use addr::parse_domain_name;
+
 pub fn hash_password(password: &str) -> String {
     let mut hasher = Sha256::new();
     hasher.input_str(password);
     hasher.result_str()
 }
 
-pub fn parse_domain(url: &str) -> Option<String> {
-    // Implement domain parsing logic
-    None
+fn parse_domain(url: &str) -> Option<String> {
+    let domain = url.trim_start_matches("http://")
+                    .trim_start_matches("https://")
+                    .trim_start_matches("www.");
+
+    let end = domain.find('/').unwrap_or(domain.len());
+    let domain = &domain[..end];
+
+    parse_domain_name(domain)
+        .ok()
+        .and_then(|parsed| parsed.root().map(String::from))
 }
